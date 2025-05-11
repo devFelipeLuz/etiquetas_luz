942557bc3a584ccdde9f6dc97d53836ad29576aa
Felipe Luz <felipetluz@outlook.com>
2025-05-11 03:26:53 -0300

    projeto finalizado

---
+++ src/scripts/main.js
@@ -0,0 +1,37 @@
+AOS.init();
+
+$(document).ready(function() {
+    
+    function updateActiveLink() {
+        
+        $('section').each(function() {
+            var sectionTop = $(this).offset().top - 100;
+            var sectionBottom = sectionTop + $(this).outerHeight();
+
+            var scrollPosition = $(window).scrollTop();
+
+            
+            if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+                var id = $(this).attr('id');
+                
+                $('.btn-menu').removeClass('active');
+                
+                $('a[href="#' + id + '"]').addClass('active');
+            }
+        });
+    }
+
+    
+    $(window).on('scroll', function() {
+        updateActiveLink();
+    });
+
+    
+    updateActiveLink();
+
+   
+    $('.btn-menu').click(function() {
+        $('.btn-menu').removeClass('active');
+        $(this).addClass('active');
+    });
+});
