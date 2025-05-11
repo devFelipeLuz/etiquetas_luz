74cfb04e59219e733054ce4ff8f7191ae695d4fd
Felipe Luz <felipetluz@outlook.com>
2025-05-11 16:42:05 -0300

    ajustando responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,42 @@
 AOS.init();
 
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     function updateActiveLink() {
-        
-        $('section').each(function() {
+
+        $('section').each(function () {
             var sectionTop = $(this).offset().top - 100;
             var sectionBottom = sectionTop + $(this).outerHeight();
 
             var scrollPosition = $(window).scrollTop();
 
-            
+
             if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
                 var id = $(this).attr('id');
-                
+
                 $('.btn-menu').removeClass('active');
-                
+
                 $('a[href="#' + id + '"]').addClass('active');
             }
         });
     }
 
-    
-    $(window).on('scroll', function() {
+
+    $(window).on('scroll', function () {
         updateActiveLink();
     });
 
-    
+
     updateActiveLink();
 
-   
-    $('.btn-menu').click(function() {
+
+    $('.btn-menu').click(function () {
         $('.btn-menu').removeClass('active');
         $(this).addClass('active');
     });
+
+    $('#menu-toggle').click(function () {
+        $('.header__menu__list').toggleClass('menu-open');
+    });
+
 });
