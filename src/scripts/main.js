73b2ea2b0538fd1ee8dd82fe027ebbedea05361e
Felipe Luz <felipetluz@outlook.com>
2025-05-13 20:08:06 -0300

    atualização de smooth de section, img e url destino para contato em contacts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,36 +3,37 @@ AOS.init();
 $(document).ready(function () {
 
     function updateActiveLink() {
-
         $('section').each(function () {
             var sectionTop = $(this).offset().top - 100;
             var sectionBottom = sectionTop + $(this).outerHeight();
-
             var scrollPosition = $(window).scrollTop();
-
 
             if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
                 var id = $(this).attr('id');
-
                 $('.btn-menu').removeClass('active');
-
                 $('a[href="#' + id + '"]').addClass('active');
             }
         });
     }
 
-
     $(window).on('scroll', function () {
         updateActiveLink();
     });
 
-
     updateActiveLink();
 
+    $('.btn-menu').click(function (e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
 
-    $('.btn-menu').click(function () {
         $('.btn-menu').removeClass('active');
         $(this).addClass('active');
+
+        var target = $(this).attr('href');
+
+        // Animação de scroll suave
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 80 // Ajuste se necessário
+        }, 600); // Duração em milissegundos (600ms)
     });
 
     $('#menu-toggle').click(function () {
